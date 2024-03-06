8c54cd2016774ccf077937b0f32de82d82318c94
Shishkin Aleksandr <shishkin.aleksadnr88@gmail.com>
2024-03-06 13:16:08 +0300

    Добавит скрипты от имени root

--- includes/scripts.rb
+++ includes/scripts.rb
@@ -1,4 +1,13 @@
 def runScripts(host, record)
+  # Выполнит скрипты из-под root
+  sudo_scripts = record["sudo_scripts"] || []
+  sudo_scripts.each do |sudo_script|
+    path = "sudo_scripts/#{sudo_script}"
+    if File.exist? path
+      host.vm.provision "shell", path: path
+    end
+  end
+
   # Выполнит скрипты из-под пользователя
   user_scripts = record["user_scripts"] || []
   user_scripts.each do |user_script|
